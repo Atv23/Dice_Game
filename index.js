5b47fc627ad27f567419d2ba07a6b10ebb929d50
Atharva <atv23shukla@gmail.com>
2023-07-15 04:43:52 +0530

    updating

--- index.js
+++ index.js
@@ -7,33 +7,33 @@ counta=Number(document.querySelector("#player1").innerHTML);
 countb=Number(document.querySelector("#player2").innerHTML);
 //console.log(a);
 if(a===1)
-    document.querySelector(".img1").setAttribute("src","/images/dice1.png");
+    document.querySelector(".img1").setAttribute("src","./images/dice1.png");
 else if(a===2)
-    document.querySelector(".img1").setAttribute("src","/images/dice2.png");
+    document.querySelector(".img1").setAttribute("src","./images/dice2.png");
 else if(a===3)
-    document.querySelector(".img1").setAttribute("src","/images/dice3.png");
+    document.querySelector(".img1").setAttribute("src","./images/dice3.png");
 else if(a===4)
-    document.querySelector(".img1").setAttribute("src","/images/dice4.png");
+    document.querySelector(".img1").setAttribute("src","./images/dice4.png");
 else if(a===5)
-    document.querySelector(".img1").setAttribute("src","/images/dice5.png");
+    document.querySelector(".img1").setAttribute("src","./images/dice5.png");
 else if(a===6)
-    document.querySelector(".img1").setAttribute("src","/images/dice6.png");
+    document.querySelector(".img1").setAttribute("src","./images/dice6.png");
 
 var b= Math.random();
 b= Math.floor(b*6 + 1);
 //console.log(b);
 if(b===1)
-    document.querySelector(".img2").setAttribute("src","/images/dice1.png");
+    document.querySelector(".img2").setAttribute("src","./images/dice1.png");
 else if(b===2)
-    document.querySelector(".img2").setAttribute("src","/images/dice2.png");
+    document.querySelector(".img2").setAttribute("src","./images/dice2.png");
 else if(b===3)
-    document.querySelector(".img2").setAttribute("src","/images/dice3.png");
+    document.querySelector(".img2").setAttribute("src","./images/dice3.png");
 else if(b===4)
-    document.querySelector(".img2").setAttribute("src","/images/dice4.png");
+    document.querySelector(".img2").setAttribute("src","./images/dice4.png");
 else if(b===5)
-    document.querySelector(".img2").setAttribute("src","/images/dice5.png");
+    document.querySelector(".img2").setAttribute("src","./images/dice5.png");
 else if(b==6)
-    document.querySelector(".img2").setAttribute("src","/images/dice6.png");
+    document.querySelector(".img2").setAttribute("src","./images/dice6.png");
 
 if(a>b)
 {
